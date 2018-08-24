cdd054718d101e4d4951e0899c5eb4ad4fa12c45
Kazuhiro Oinuma <oinume@gmail.com>
2018-08-24 16:22:50 +0900

    Add exponential_backoff

---
+++ exponential_backoff/exponential_backoff.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/cenkalti/backoff"
+)
+
+func main() {
+	expBackoff := backoff.NewExponentialBackOff()
+	expBackoff.Multiplier = 2.5
+	expBackoff.InitialInterval = 3000 * time.Millisecond
+	expBackoff.RandomizationFactor = 0.0
+	expBackoff.MaxElapsedTime = 20 * time.Second
+	expBackoff.MaxInterval = 15 * time.Second
+	backoff.RetryNotify(func() error {
+		fmt.Printf("Operation()\n")
+		return fmt.Errorf("error!")
+	}, expBackoff, func(err error, d time.Duration) {
+		fmt.Printf("notifiy: err=%v, d = %v\n", err, d)
+	})
+}
