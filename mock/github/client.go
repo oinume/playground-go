71102e276a536b26d9679e6e139f5000f8be1359
Kazuhiro Oinuma <oinume@gmail.com>
2024-06-17 17:00:30 +0900

    Rename generated files

--- mock/github/client.go
+++ mock/github/client.go
@@ -2,8 +2,8 @@ package github
 
 import "context"
 
-//go:generate go run github.com/matryer/moq -out=client_test.moq.go . Client
-//go:generate go run go.uber.org/mock/mockgen -destination=client_test.gomock.go -package=github . Client
+//go:generate go run github.com/matryer/moq -out=client.moq.go . Client
+//go:generate go run go.uber.org/mock/mockgen -destination=client.gomock.go -package=github . Client
 
 type Client interface {
 	ListBranches(ctx context.Context, owner, repo string) ([]string, error)
